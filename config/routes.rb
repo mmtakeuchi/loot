44c241a3e98437b34d770a862b3cde3205fc2abb
mmtakeuchi <mikemtaki@gmail.com>
2020-08-17 15:11:35 -0700

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :stocks
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  root "static#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
